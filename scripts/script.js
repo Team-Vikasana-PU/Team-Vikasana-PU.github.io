4bf67ee86735031ce6510edcd68d9afc7fa2a68b
Umraz <kumraz858@gmail.com>
2022-04-05 19:56:40 +0530

    workshop update

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,20 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+$(".notch").click(() => {
+    let popup = $("#popup")
+    if(popup.hasClass('popup-active')) {
+        $("#popup").removeClass('popup-active')
+    }
+    else {
+        $("#popup").addClass('popup-active')
+    }  
+})
+
+const mediaQuery = window.matchMedia('(max-width: 1280px)')
+    if (mediaQuery.matches) {
+        if($("#popup").hasClass('popup-active')) {
+            $('#popup').removeClass('popup-active')
+        }
+    }
